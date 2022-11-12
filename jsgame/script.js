3078d2b0a94818223f5b0eeede7c3b6dcc2577b0
nursaadat <SNursultan@dar.kz>
2022-11-12 20:28:58 +0100

    add(jsgame): added dog sprite to the canvas

--- jsgame/script.js
+++ jsgame/script.js
@@ -1,15 +1,38 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-// console.log(ctx);
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.fillRect(50, 50, 100, 100);
+  // ctx.fillRect(50, 50, 100, 100);
+  // cts.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+  ctx.drawImage(
+    playerImage,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT
+  );
+  if (gameFrame % staggerFrames == 0) {
+    if (frameX < 6) frameX++;
+    else frameX = 0;
+  }
+
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
