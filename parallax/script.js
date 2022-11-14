9824740b2b067763d3beded51c351977d9cc1d07
nursaadat <SNursultan@dar.kz>
2022-11-14 13:05:46 +0100

    add(parallax): added 5 layers to the background

--- parallax/script.js
+++ parallax/script.js
@@ -1,12 +1,10 @@
 const canvas = document.getElementById('canvas1');
-// context is specific to the canvas tag
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d'); // context is specific to the canvas tag
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
-let gameSpeed = 15;
+let gameSpeed = 10;
 
-// new Image() is same as document.createElement("img")
-const backgroundLayer1 = new Image();
+const backgroundLayer1 = new Image(); // new Image() is same as document.createElement("img")
 backgroundLayer1.src = 'layer-1.png';
 const backgroundLayer2 = new Image();
 backgroundLayer2.src = 'layer-2.png';
@@ -45,12 +43,20 @@ class Layer {
   }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  layer4.update();
-  layer4.draw();
+  gameObjects.forEach((object) => {
+    object.update();
+    object.draw();
+  });
   requestAnimationFrame(animate);
 }
 animate();
