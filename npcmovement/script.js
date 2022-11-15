57ad0ec076ea1d3f15ba8ea4ca5bc10124af2e52
nursaadat <SNursultan@dar.kz>
2022-11-15 11:02:34 +0100

    add(npcmovement): created base canvas

---
+++ npcmovement/script.js
@@ -0,0 +1,31 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 100;
+    this.height = 100;
+  }
+  update() {
+    this.x++;
+    this.y++;
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+const enemy1 = new Enemy();
+const enemy2 = new Enemy();
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemy1.update();
+  enemy1.draw();
+  requestAnimationFrame(animate);
+}
+animate();
