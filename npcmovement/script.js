eca7ab258087e68e50c33c418c663de99a8e37ff
nursaadat <SNursultan@dar.kz>
2022-11-19 11:28:41 +0100

    add(npcmovement): added flower game base

--- npcmovement/script.js
+++ npcmovement/script.js
@@ -3,63 +3,66 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 100;
-const enemiesArray = [];
+const numberOfFlowers = 20;
+const floatingFlowers = [];
 
-let gameFrame = 0;
+var cursor = document.getElementById('cursor');
+document.addEventListener('mousemove', function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+});
 
-class Enemy {
+class Flower {
   constructor() {
     this.image = new Image();
-    this.image.src = 'enemy2.png';
-    this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 266;
-    this.spriteHeight = 188;
-    this.width = this.spriteWidth / 2.5;
-    this.height = this.spriteHeight / 2.5;
-    this.x = Math.random() * (canvas.width - this.width);
-    this.y = Math.random() * (canvas.height - this.height);
-    this.frame = 0;
-    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = 0;
-    this.angleSpeed = Math.random() * 0.2;
-    this.curve = Math.random() * 7;
-  }
-  update() {
-    this.x -= this.speed;
-    this.y += this.curve * Math.sin(this.angle);
-    this.angle += this.angleSpeed;
-    if (this.x + this.width < 0) this.x = canvas.width;
-    if (gameFrame % this.flapSpeed === 0) {
-      this.frame > 4 ? (this.frame = 0) : this.frame++;
-    }
+    this.image.src = 'flower.png';
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.spriteSize = Math.random() * 50 + 30;
+    this.speed = Math.random() * 3 + 1;
+    this.angle = Math.random() * 360;
+    this.spin = Math.random() < 0.5 ? -1 : 1;
   }
   draw() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(((this.angle * Math.PI) / 360) * this.spin);
     ctx.drawImage(
       this.image,
-      this.frame * this.spriteWidth,
-      0,
-      this.spriteWidth,
-      this.spriteHeight,
-      this.x,
-      this.y,
-      this.width,
-      this.height
+      0 - this.spriteSize / 2,
+      0 - this.spriteSize / 2,
+      this.spriteSize,
+      this.spriteSize
     );
+    ctx.restore();
+  }
+  update() {
+    this.y += this.speed;
+    this.angle++;
+    if (this.y > canvas.height) {
+      this.y = 0 - this.spriteSize;
+      this.x = Math.random() * canvas.width;
+      this.spriteSize = Math.random() * 50 + 30;
+      this.speed = Math.random() * 3 + 1;
+    }
   }
 }
 
-for (let i = 0; i < numberOfEnemies; i++) {
-  enemiesArray.push(new Enemy());
+function init() {
+  for (let i = 0; i < numberOfFlowers; i++) {
+    floatingFlowers.push(new Flower());
+  }
 }
+init();
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemiesArray.forEach((enemy) => {
-    enemy.update();
-    enemy.draw();
+  floatingFlowers.forEach((flower) => {
+    flower.update();
+    flower.draw();
   });
-  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
